84c6e79937752ee57d361264e79189e4c537e0ef
Darren Murray <darren.murray@lacework.net>
2021-08-12 18:22:31 +0100

    fix: Up items per page on repo url
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- api/api.go
+++ api/api.go
@@ -1,7 +1,7 @@
 package api
 
 const (
-	REPOS_URL           = "orgs/lacework/teams/tech-ally/repos"
+	REPOS_URL           = "orgs/lacework/teams/tech-ally/repos?page=1&per_page=60"
 	PR_URL              = "repos/lacework/%s/pulls?state=open"
 	ISSUES_URL          = "repos/lacework/%s/issues?state=open"
 	LATEST_RELEASES_URL = "repos/lacework/%s/releases/latest"
